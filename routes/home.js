69f792760ce4ce1d807b9dc5ee6251e626b8ada7
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-02-14 10:35:16 +0200

    set up signup post route to create users

--- routes/home.js
+++ routes/home.js
@@ -13,4 +13,43 @@ router.get('/signup', function (req, res, next) {
   res.render('signup', { title: 'Sign up Page' })
 })
 
+router.post('/signup', [
+  query('username').trim().escape(),
+  query('password').trim().escape(),
+  query('confirm_password').trim().escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+
+    if (req.body.password !== req.body.confirm_password) {
+      res.render('signup', {
+        title: 'Sign up Page',
+        errors: [{ msg: 'Passwords do not match' }],
+      })
+      return
+    }
+
+    if (!errors.isEmpty()) {
+      res.render('signup', {
+        title: 'Sign up Form',
+        errors: errors.array(),
+      })
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const user = new User({
+      username: req.body.username,
+      password: hashedPassword,
+      member_status: 'Member',
+      date_joined: new Date(),
+    })
+    try {
+      await user.save()
+      res.redirect('/home')
+    } catch (err){
+      console.log(err)
+    }
+  }),
+])
+
 module.exports = router
