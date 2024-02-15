f68e541a6a01d59d36cd2926a37ef58b1f695618
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-02-15 16:39:40 +0200

    added admin property

--- models/userSchema.js
+++ models/userSchema.js
@@ -6,6 +6,7 @@ const UserSchema = new Schema({
   password: { type: String, required: true },
   member_status: { type: String, required: true },
   date_joined: { type: Date, default: Date.now, required: true },
+  admin: { type: Boolean, default: false },
 })
 
 module.exports = mongoose.model('User', UserSchema)
